699474a790c65e018708f9c6513bb561fcc5f836
TylerRLofton <loftont1@my.erau.edu>
2024-10-22 11:46:07 -0400

    Created boxes without using individual canvases

--- script.js
+++ script.js
@@ -1,58 +1,44 @@
 window.onload = function () {
     var outerCanvas = document.getElementById("outerCanvas");
-    var graphCanvas = document.getElementById("graphCanvas");
-    var constellationCanvas = document.getElementById("constellationCanvas");
-    var statusCanvas = document.getElementById("statusCanvas");
-    var activeStatusCanvas = document.getElementById("activeStatusCanvas");
-    var raimCanvas = document.getElementById("raimCanvas");
-    if (outerCanvas &&
-        graphCanvas &&
-        constellationCanvas &&
-        statusCanvas &&
-        activeStatusCanvas &&
-        raimCanvas) {
+    if (outerCanvas) {
         var outerCtx = outerCanvas.getContext("2d");
-        var graphCtx = graphCanvas.getContext("2d");
-        var constellationCtx = constellationCanvas.getContext("2d");
-        var statusCtx = statusCanvas.getContext("2d");
-        var activeStatusCtx = activeStatusCanvas.getContext("2d");
-        var raimCtx = raimCanvas.getContext("2d");
-        if (outerCtx &&
-            graphCtx &&
-            constellationCtx &&
-            statusCtx &&
-            activeStatusCtx &&
-            raimCtx) {
+        if (outerCtx) {
             outerCanvas.width = outerCanvas.clientWidth;
             outerCanvas.height = outerCanvas.clientHeight;
-            graphCanvas.width = graphCanvas.clientWidth;
-            graphCanvas.height = graphCanvas.clientHeight;
-            constellationCanvas.width = constellationCanvas.clientWidth;
-            constellationCanvas.height = constellationCanvas.clientHeight;
-            statusCanvas.width = statusCanvas.clientWidth;
-            statusCanvas.height = statusCanvas.clientHeight;
-            activeStatusCanvas.width = activeStatusCanvas.clientWidth;
-            activeStatusCanvas.height = activeStatusCanvas.clientHeight;
+            outerCanvas.style.position = "absolute";
+            outerCanvas.style.top = "0";
+            outerCanvas.style.left = "0";
+            outerCanvas.style.backgroundColor = "green";
+            //Constellation
+            outerCtx.fillStyle = "red";
+            outerCtx.fillRect(0, 0, 466, 525);
+            outerCtx.strokeStyle = "black";
+            outerCtx.lineWidth = 1;
+            outerCtx.strokeRect(0, 0, 466, 525);
+            //Satellite Status
+            outerCtx.fillStyle = "blue";
+            outerCtx.fillRect(467, 0, 466, 525);
+            outerCtx.strokeStyle = "black";
+            outerCtx.lineWidth = 1;
+            outerCtx.strokeRect(467, 0, 466, 525);
+            //Active GPS Status
+            outerCtx.fillStyle = "yellow";
+            outerCtx.fillRect(934, 0, 466, 262);
+            outerCtx.strokeStyle = "black";
+            outerCtx.lineWidth = 1;
+            outerCtx.strokeRect(934, 0, 466, 262);
+            //RAIM Prediction
+            outerCtx.fillStyle = "purple";
+            outerCtx.fillRect(934, 263, 466, 262);
+            outerCtx.strokeStyle = "black";
+            outerCtx.lineWidth = 1;
+            outerCtx.strokeRect(934, 263, 466, 262);
+            //GPS Signal Strength
+            outerCtx.fillStyle = "orange";
+            outerCtx.fillRect(0, 525, 1400, 525);
+            outerCtx.strokeStyle = "black";
+            outerCtx.lineWidth = 1;
+            outerCtx.strokeRect(0, 525, 1400, 525);
         }
-        outerCanvas.style.position = "absolute";
-        outerCanvas.style.top = "0";
-        outerCanvas.style.left = "0";
-        outerCanvas.style.backgroundColor = "green";
-        graphCanvas.style.position = "absolute";
-        graphCanvas.style.top = "50%";
-        graphCanvas.style.left = "0";
-        graphCanvas.style.backgroundColor = "blue";
-        constellationCanvas.style.position = "absolute";
-        constellationCanvas.style.top = "0";
-        constellationCanvas.style.left = "0";
-        constellationCanvas.style.backgroundColor = "red";
-        // statusCanvas.style.position = "absolute";
-        // statusCanvas.style.top = "0";
-        // statusCanvas.style.left = "0";
-        // statusCanvas.style.backgroundColor = "white";
-        // activeStatusCanvas.style.position = "absolute";
-        // activeStatusCanvas.style.top = "50%";
-        // activeStatusCanvas.style.left = "0";
-        // activeStatusCanvas.style.backgroundColor = "pink";
     }
 };
--- script.ts
+++ script.ts
@@ -1,83 +1,51 @@
 window.onload = () => {
-  const outerCanvas = document.getElementById(
-    "outerCanvas",
-  ) as HTMLCanvasElement;
-  const graphCanvas = document.getElementById(
-    "graphCanvas",
-  ) as HTMLCanvasElement;
-  const constellationCanvas = document.getElementById(
-    "constellationCanvas",
-  ) as HTMLCanvasElement;
-  const statusCanvas = document.getElementById(
-    "statusCanvas",
-  ) as HTMLCanvasElement;
-  const activeStatusCanvas = document.getElementById(
-    "activeStatusCanvas",
-  ) as HTMLCanvasElement;
-  const raimCanvas = document.getElementById("raimCanvas") as HTMLCanvasElement;
+  const outerCanvas = document.getElementById("outerCanvas") as HTMLCanvasElement;
 
-  if (
-    outerCanvas &&
-    graphCanvas &&
-    constellationCanvas &&
-    statusCanvas &&
-    activeStatusCanvas &&
-    raimCanvas
-  ) {
+  if (outerCanvas) {
     const outerCtx = outerCanvas.getContext("2d");
-    const graphCtx = graphCanvas.getContext("2d");
-    const constellationCtx = constellationCanvas.getContext("2d");
-    const statusCtx = statusCanvas.getContext("2d");
-    const activeStatusCtx = activeStatusCanvas.getContext("2d");
-    const raimCtx = raimCanvas.getContext("2d");
 
-    if (
-      outerCtx &&
-      graphCtx &&
-      constellationCtx &&
-      statusCtx &&
-      activeStatusCtx &&
-      raimCtx
-    ) {
+    if (outerCtx) {
       outerCanvas.width = outerCanvas.clientWidth;
       outerCanvas.height = outerCanvas.clientHeight;
+      outerCanvas.style.position = "absolute";
+      outerCanvas.style.top = "0";
+      outerCanvas.style.left = "0";
+      outerCanvas.style.backgroundColor = "green";
 
-      graphCanvas.width = graphCanvas.clientWidth;
-      graphCanvas.height = graphCanvas.clientHeight;
+      //Constellation
+      outerCtx.fillStyle = "red";
+      outerCtx.fillRect(0, 0, 466, 525);
+      outerCtx.strokeStyle = "black";
+      outerCtx.lineWidth = 1;
+      outerCtx.strokeRect(0, 0, 466, 525);
 
-      constellationCanvas.width = constellationCanvas.clientWidth;
-      constellationCanvas.height = constellationCanvas.clientHeight;
+      //Satellite Status
+      outerCtx.fillStyle = "blue";
+      outerCtx.fillRect(467, 0, 466, 525);
+      outerCtx.strokeStyle = "black";
+      outerCtx.lineWidth = 1;
+      outerCtx.strokeRect(467, 0, 466, 525);
 
-      statusCanvas.width = statusCanvas.clientWidth;
-      statusCanvas.height = statusCanvas.clientHeight;
+      //Active GPS Status
+      outerCtx.fillStyle = "yellow";
+      outerCtx.fillRect(934, 0, 466, 262);
+      outerCtx.strokeStyle = "black";
+      outerCtx.lineWidth = 1;
+      outerCtx.strokeRect(934, 0, 466, 262);
 
-      activeStatusCanvas.width = activeStatusCanvas.clientWidth;
-      activeStatusCanvas.height = activeStatusCanvas.clientHeight;
+      //RAIM Prediction
+      outerCtx.fillStyle = "purple";
+      outerCtx.fillRect(934, 263, 466, 262);
+      outerCtx.strokeStyle = "black";
+      outerCtx.lineWidth = 1;
+      outerCtx.strokeRect(934, 263, 466, 262);
+
+      //GPS Signal Strength
+      outerCtx.fillStyle = "orange";
+      outerCtx.fillRect(0, 525, 1400, 525);
+      outerCtx.strokeStyle = "black";
+      outerCtx.lineWidth = 1;
+      outerCtx.strokeRect(0, 525, 1400, 525);
     }
-
-    outerCanvas.style.position = "absolute";
-    outerCanvas.style.top = "0";
-    outerCanvas.style.left = "0";
-    outerCanvas.style.backgroundColor = "green";
-
-    graphCanvas.style.position = "absolute";
-    graphCanvas.style.top = "50%";
-    graphCanvas.style.left = "0";
-    graphCanvas.style.backgroundColor = "blue";
-
-    constellationCanvas.style.position = "absolute";
-    constellationCanvas.style.top = "0";
-    constellationCanvas.style.left = "0";
-    constellationCanvas.style.backgroundColor = "red";
-
-    // statusCanvas.style.position = "absolute";
-    // statusCanvas.style.top = "0";
-    // statusCanvas.style.left = "0";
-    // statusCanvas.style.backgroundColor = "white";
-
-    // activeStatusCanvas.style.position = "absolute";
-    // activeStatusCanvas.style.top = "50%";
-    // activeStatusCanvas.style.left = "0";
-    // activeStatusCanvas.style.backgroundColor = "pink";
   }
 };
