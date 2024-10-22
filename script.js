cc3dff381bcf56b12d60e291a35141cddfc0eb9c
TylerRLofton <loftont1@my.erau.edu>
2024-10-22 11:57:55 -0400

    Added labels to each box

--- script.js
+++ script.js
@@ -8,37 +8,52 @@ window.onload = function () {
             outerCanvas.style.position = "absolute";
             outerCanvas.style.top = "0";
             outerCanvas.style.left = "0";
-            outerCanvas.style.backgroundColor = "green";
+            outerCanvas.style.backgroundColor = "black";
             //Constellation
-            outerCtx.fillStyle = "red";
+            outerCtx.fillStyle = "white";
             outerCtx.fillRect(0, 0, 466, 525);
             outerCtx.strokeStyle = "black";
             outerCtx.lineWidth = 1;
             outerCtx.strokeRect(0, 0, 466, 525);
+            outerCtx.fillStyle = "black";
+            outerCtx.font = "30px Arial";
+            outerCtx.fillText("Constellation", 150, 50);
             //Satellite Status
-            outerCtx.fillStyle = "blue";
+            outerCtx.fillStyle = "white";
             outerCtx.fillRect(467, 0, 466, 525);
             outerCtx.strokeStyle = "black";
             outerCtx.lineWidth = 1;
             outerCtx.strokeRect(467, 0, 466, 525);
+            outerCtx.fillStyle = "black";
+            outerCtx.font = "30px Arial";
+            outerCtx.fillText("Satellite Status", 600, 50);
             //Active GPS Status
-            outerCtx.fillStyle = "yellow";
+            outerCtx.fillStyle = "white";
             outerCtx.fillRect(934, 0, 466, 262);
             outerCtx.strokeStyle = "black";
             outerCtx.lineWidth = 1;
             outerCtx.strokeRect(934, 0, 466, 262);
+            outerCtx.fillStyle = "black";
+            outerCtx.font = "30px Arial";
+            outerCtx.fillText("Active GPS Status", 1050, 50);
             //RAIM Prediction
-            outerCtx.fillStyle = "purple";
+            outerCtx.fillStyle = "white";
             outerCtx.fillRect(934, 263, 466, 262);
             outerCtx.strokeStyle = "black";
             outerCtx.lineWidth = 1;
             outerCtx.strokeRect(934, 263, 466, 262);
+            outerCtx.fillStyle = "black";
+            outerCtx.font = "30px Arial";
+            outerCtx.fillText("RAIM Prediction", 1050, 313);
             //GPS Signal Strength
-            outerCtx.fillStyle = "orange";
+            outerCtx.fillStyle = "white";
             outerCtx.fillRect(0, 525, 1400, 525);
             outerCtx.strokeStyle = "black";
             outerCtx.lineWidth = 1;
             outerCtx.strokeRect(0, 525, 1400, 525);
+            outerCtx.fillStyle = "black";
+            outerCtx.font = "30px Arial";
+            outerCtx.fillText("GPS Signal Strength", 600, 575);
         }
     }
 };
