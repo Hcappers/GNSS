2920ae9712e5e8fba16420bfa8436a86087e6b1f
TylerRLofton <loftont1@my.erau.edu>
2024-10-31 15:23:07 -0400

    Reformated code to make it more readable

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var canvas = document.getElementById("outerCanvas");
 var ctx = canvas.getContext("2d");
 var width = canvas.clientWidth;
 var height = canvas.clientHeight;
+//######################
+//##  Init Functions  ##
+//######################
 function initCanvas() {
     if (ctx) {
         canvas.width = canvas.clientWidth;
@@ -10,6 +13,26 @@ function initCanvas() {
         canvas.style.top = "0";
         canvas.style.left = "0";
         canvas.style.backgroundColor = "black";
+    }
+}
+function initBoxes() {
+    drawBox(0, 0, 466, 525, "#2f2f2f"); //Constellation
+    drawBox(467, 0, 466, 525, "#2f2f2f"); //Satellite Status
+    drawBox(934, 0, 466, 262, "#2f2f2f"); //Active GPS Status
+    drawBox(934, 263, 466, 262, "#2f2f2f"); //RAIM Prediction
+    drawBox(0, 525, 1400, 525, "#2f2f2f"); //GPS Signal Strength
+}
+//######################
+//##  Util Functions  ##
+//######################
+function drawLine(x1, x2, y1, y2, width, color) {
+    if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.stroke();
     }
 }
 function drawBox(x, y, width, height, color) {
@@ -52,30 +75,9 @@ function addText(x, y, text, fontSize) {
         ctx.fillText(text, x, y);
     }
 }
-function activeGPSStatus() {
-    addText(945, 30, "Active GPS Status", 25);
-    addText(945, 70, "Pilot", 20);
-    addText(945, 110, "Copilot", 20);
-    addText(945, 150, "Status", 20);
-    addText(945, 190, "SBAS", 20);
-}
-function raimPrediction() {
-    addText(945, 290, "RAIM Prediction", 25);
-    addText(945, 330, "Waypoint", 20);
-    addText(945, 370, "ARV Time", 20);
-    addText(945, 410, "ARV Date", 20);
-}
-function gpsSignalStrength() {
-    addText(50, 560, "GPS Signal Strength", 25);
-    drawBox(50, 575, 1300, 425, "#2f2f2f");
-}
-function initBoxes() {
-    drawBox(0, 0, 466, 525, "#2f2f2f"); //Constellation
-    drawBox(467, 0, 466, 525, "#2f2f2f"); //Satellite Status
-    drawBox(934, 0, 466, 262, "#2f2f2f"); //Active GPS Status
-    drawBox(934, 263, 466, 262, "#2f2f2f"); //RAIM Prediction
-    drawBox(0, 525, 1400, 525, "#2f2f2f"); //GPS Signal Strength
-}
+//########################
+//##  Window Functions  ##
+//########################
 function constellation() {
     drawRing(200, 200, 100, 30);
 }
@@ -100,6 +102,23 @@ function satelliteStatus() {
     addText(800, 390, "____._KT", 20);
     addText(480, 430, "Track", 20);
     addText(800, 430, "___\u00B0", 20);
+}
+function activeGPSStatus() {
+    addText(945, 30, "Active GPS Status", 25);
+    addText(945, 70, "Pilot", 20);
+    addText(945, 110, "Copilot", 20);
+    addText(945, 150, "Status", 20);
+    addText(945, 190, "SBAS", 20);
+}
+function raimPrediction() {
+    addText(945, 290, "RAIM Prediction", 25);
+    addText(945, 330, "Waypoint", 20);
+    addText(945, 370, "ARV Time", 20);
+    addText(945, 410, "ARV Date", 20);
+}
+function gpsSignalStrength() {
+    addText(50, 560, "GPS Signal Strength", 25);
+    drawBox(50, 575, 1300, 425, "#2f2f2f");
 }
 initCanvas();
 initBoxes();
